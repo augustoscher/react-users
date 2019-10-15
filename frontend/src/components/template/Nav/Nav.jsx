1678094b6914c8ea6dc05c89dafe2df0b33d00a4
Augusto Scher <augusto.scher@resultadosdigitais.com.br>
2019-10-15 20:14:15 -0300

    nav component pt1

--- frontend/src/components/template/Nav/Nav.jsx
+++ frontend/src/components/template/Nav/Nav.jsx
@@ -3,5 +3,13 @@ import React from 'react';
 
 export default props =>
   <aside className='menu-area'>
-    Nav
+    <nav className="menu">
+      {/* Refactor to NavItem component */}
+      <a href="#/">
+        <i className="fa fa-home"></i> Início
+      </a>
+      <a href="#/">
+        <i className="fa fa-user"></i> Usuários
+      </a>
+    </nav>
   </aside>
